d982fb7b9157d09d9798cad89f289d928d31a936
Matthew Thomson <m7homson@gmail.com>
2017-07-15 00:03:15 -0300

    re-writing the whole thing for more ephemeral-ness.

--- deepen.py
+++ deepen.py
@@ -5,29 +5,20 @@ from dependancies.deps import get_deps
 from pymongo import MongoClient
 
 app = Flask(__name__)
-
 db = MongoClient('localhost:27017').deepen
 
-@app.route('/projects', methods=['GET'])
-def new_project():
-    return render_template('new.html', api=request.url_root)
+@app.route('/', methods=['GET'])
+def get_depend():
+    url = request.args.get('url')
+    print(url, file=sys.stderr)
+    if not url:
+        return render_template('new.html', api=request.url_root)
 
-@app.route('/projects/<name>')
-def show_project(name):
-    matching_projects = list(db.projects.find({'name': name}))
-    return render_template('project.html', project=matching_projects[0])
+    new_project = {
+        'repo_url': url,
+        'deps': compare_requirements(get_deps(url))
+    }
+    db.projects.insert_one(new_project)
+    return render_template('project.html', project=new_project)
 
 
-@app.route('/projects', methods=['POST'])
-def create_project():
-    data = dict((key, request.form.getlist(key)) for key in request.form.keys())
-    name = data['name'][0]
-    url = data['url'][0]
-    db.projects.insert_one(
-        {
-            'name': name,
-            'repo_url': url,
-            'deps': compare_requirements(get_deps(url))
-        }
-    )
-    return redirect("projects/{}".format(name), code=302)
